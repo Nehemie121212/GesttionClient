43ad3df9075c2afeb42b760843ed4dcad6fc31f9
zahrneh <nehemie.zahrane@gmail.com>
2023-01-24 14:21:36 +0100

    Initial commit

---
+++ quasar-GestCli/src/layouts/MainLayout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          aria-label="Menu"
+          dense
+          flat
+          icon="menu"
+          round
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      show-if-above
+    >
+      <q-list>
+        <q-list>
+          <q-item-label
+            class="text-grey-8"
+            header
+          >
+            Menu de navigation
+          </q-item-label>
+          <q-item clickable exact to="/">
+            <q-item-section avatar>
+              <q-icon name="home"/>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>Accueil</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable exact to="/clients">
+            <q-item-section avatar>
+              <q-icon name="person"/>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>Clients</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  },
+  methods: {
+    toggleLeftDrawer () {
+      this.leftDrawerOpen = !this.leftDrawerOpen
+    }
+  }
+}
+</script>
